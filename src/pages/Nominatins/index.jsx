984d99ce8b233d82f3f2bae16447e16c55ade84c
maidersonn <maidersonn@gmail.com>
2022-06-15 23:44:27 +0200

    feat: first display of data in Nominations page

--- src/pages/Nominatins/index.jsx
+++ src/pages/Nominatins/index.jsx
@@ -1,6 +1,27 @@
+import { useState, useEffect } from "react";
+import { getAllNonRejectedNominations } from "../../services"
+
 const Nominations = () => {
+    const [nonRejectedNomination, setNonRejectedNominations] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            console.log("hola")
+            const results = await getAllNonRejectedNominations();
+            setNonRejectedNominations(results.data);
+        };
+        fetchData();
+    }, []);
+
+
     return (
-        <p>Nominations will be shown here</p>
+        <section>
+            {nonRejectedNomination.map((nomination, index) => {
+                return (
+                    <p key={index}>{nomination.email}</p>
+                )
+            })}
+        </section>
     )
 };
 
