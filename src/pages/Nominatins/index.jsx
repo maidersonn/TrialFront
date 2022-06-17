527aa6d2cde2847aafeb9d0b768dfc1a9221be6c
maidersonn <maidersonn@gmail.com>
2022-06-17 11:04:12 +0200

    feat: build nomination page

--- src/pages/Nominatins/index.jsx
+++ src/pages/Nominatins/index.jsx
@@ -1,28 +1,64 @@
 import { useState, useEffect } from "react";
 import { getAllNonRejectedNominations } from "../../services"
+import "./nominations.css";
 
 const Nominations = () => {
     const [nonRejectedNomination, setNonRejectedNominations] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
-            console.log("hola")
             const results = await getAllNonRejectedNominations();
             setNonRejectedNominations(results.data);
         };
         fetchData();
     }, []);
 
-
     return (
-        <section>
-            {nonRejectedNomination.map((nomination, index) => {
-                return (
-                    <p key={index}>{nomination.email}</p>
-                )
-            })}
+        <section className="nominations">
+            <h1 className="nominations_title">This is the list of non rejected nominations.</h1>
+            <table className="table">
+                <colgroup>
+                    <col className="email_column"></col>
+                    <col className="description_column"></col>
+                    <col className="talent_column"></col>
+                    <col className="involvement_column"></col>
+                    <col className="status_column"></col>
+                    <col className="date_column"></col>
+
+
+                </colgroup>
+                <thead className="table_headings">
+                    <tr className="table_headings_row">
+                        <th>Email</th>
+                        <th>Description</th>
+                        <th>Talent</th>
+                        <th>Involvement</th>
+                        <th>Status</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody >
+                    {nonRejectedNomination.map(({ email, description, score, status, dateReferred }, index) => {
+                        return (
+
+                            <tr className="table_body_row" key={index}>
+                                <td>{email}</td>
+                                <td>{description}</td>
+                                <td>{score.talent}</td>
+                                <td>{score.involvement}</td>
+                                <td>{status}</td>
+                                <td>{formatDate(dateReferred)}</td>
+                            </tr>
+
+                        )
+                    })}
+                </tbody>
+            </table>
+
         </section>
     )
 };
+
+const formatDate = (string) => string.slice(0, 10);
 
 export default Nominations;
