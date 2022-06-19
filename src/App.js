27cfe168923748650a941c60c1c8d33db2b1ff0e
maidersonn <maidersonn@gmail.com>
2022-06-19 23:14:31 +0200

    feat: add Home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 
 import Nominations from "./pages/Nominatins";
-import Form from "./pages/Form";
+import CreateNominations from "./pages/CreateNominations";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <Routes>
+      <Route path={"/"} element={<Home />} exact={true} />
       <Route path={"/nominations"} element={<Nominations />} exact={true} />
-      <Route path={"/nominations/new"} element={<Form />} exact={true} />
+      <Route path={"/nominations/new"} element={<CreateNominations />} exact={true} />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here! Check type errors in url</p>
+          </main>
+        }
+      />
     </Routes>
   );
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import "./home.css";
+
+const Home = () => {
+    return (
+        <div className="home_page">
+            <header className="home_page_header">TRIAL</header>
+            <h1 className="home_page_wellcome">Wellcome to my coding trial !</h1>
+            <p className="home_page_info">Here you can find a list of nominations and also you have the posibility to make new nominations.</p>
+            <p className="home_page_info">Remember that only non rejected nominations are shown.</p>
+        </div>
+
+    );
+};
+
+export default Home;
